337042539d9607064b8fddc8b754fbccbc2da17a
Taras <hyrkat@users.noreply.github.com>
2025-10-15 15:08:53 +0300

    all

---
+++ test_first.py
@@ -0,0 +1,11 @@
+from playwright.sync_api import sync_playwright
+
+def run():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)  # Set headless=True to run without opening a browser window
+        page = browser.new_page()
+        page.goto("https://www.google.com")
+        print("Page title:", page.title())
+        browser.close()
+
+run()
